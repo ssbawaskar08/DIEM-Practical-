15965189ebb98b13be0761ca6a822dd92307e9be
Shreyas <shreyasbawaskar0812@gmail.com>
2025-03-17 11:42:18 +0530

    formatting question

--- Python-Programming/17-03-25/question3.py
+++ Python-Programming/17-03-25/question3.py
@@ -1,6 +1,6 @@
 # variable name balance = 1000/- 2nd variable is amount(for withdrawal):
-#  condition1 if amount grater than balance then display insuffiecient balance
-#  condition2 amount should be the multiples of 100
+#  condition1 if amount greater than balance then display insuffiecient balance
+#  condition2 amount should be the multiples of 10
 #  condition3 if the balance - amount after successful transaction and display current balance 
 balance = 1000
 amount = int(input("Enter the Amount you wish to withdraw: "))
